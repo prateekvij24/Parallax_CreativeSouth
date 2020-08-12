ac16c2a2bdc031a09ebf8d9d3527aec48a4b6a7f
Prateek VIJ <prateek.vij24@gmail.com>
2020-08-12 19:49:24 +0530

    Removed Scrolling issues

--- lib/main.dart
+++ lib/main.dart
@@ -21,60 +21,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  MediaQueryData data;
   double opac = 0.1;
-  double pos = -100;
+  double posleft2 = -200;
+  double postop1 = 0;
+  double postop2 = 0;
+  double postop3 = 0;
   @override
   Widget build(BuildContext context) {
+    final data = MediaQuery.of(context);
     return Scaffold(
       body: NotificationListener(
         onNotification: (notification) {
           if (notification is ScrollUpdateNotification) {
             setState(() {
-              opac = opac + notification.scrollDelta / 200;
-              pos = pos + notification.scrollDelta / 2;
+              opac = opac + notification.scrollDelta / 150;
+              posleft2 = posleft2 + notification.scrollDelta / 2;
+              postop1 = postop1 - notification.scrollDelta / 1;
+              postop2 = postop2 - notification.scrollDelta / 1;
+              postop3 = postop3 - notification.scrollDelta / 1;
               if (opac <= 1 && opac >= 0.1)
                 opac = opac;
               else if (opac < 0.1)
                 opac = 0.1;
               else
                 opac = 1;
-              if (pos >= -100 && pos <= 0)
-                pos = pos;
-              else if (pos < -100)
-                pos = -100;
+              if (posleft2 >= -200 && posleft2 <= -25)
+                posleft2 = posleft2;
+              else if (posleft2 < -200)
+                posleft2 = -200;
               else
-                pos = 0;
+                posleft2 = -25;
             });
           }
         },
         child: Stack(children: [
-          Opacity(
-            opacity: opac,
+          Positioned(
+            top: postop1,
+            child: Opacity(
+              opacity: opac,
+              child: Container(
+                height: 600,
+                width: data.size.width,
+                child: Image.asset(
+                  'assets/images/image1.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: postop2,
             child: Container(
-              height: 500,
-              width: double.infinity,
+              height: 600,
+              width: data.size.width,
               child: Image.asset(
-                'assets/images/image1.png',
+                'assets/images/image2.png',
                 fit: BoxFit.cover,
               ),
             ),
           ),
-          Container(
-            height: 500,
-            width: double.infinity,
-            child: Image.asset(
-              'assets/images/image2.png',
-              fit: BoxFit.cover,
-            ),
-          ),
           Positioned(
+              top: postop2,
               child: Container(
-                height: 500,
+                width: data.size.width,
+                height: 600,
                 child: Image.asset(
                   'assets/images/image3.png',
                 ),
               ),
-              left: pos),
+              left: posleft2),
           ListView(
             children: [
               Container(
